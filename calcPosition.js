e3c3ed171d1219d94d8d4399cdf8131eccf9a71b
aunikitin <sanya_nikiti@mail.ru>
2018-04-08 07:40:41 +0500

    наконец-то это дерьмо работает

--- calcPosition.js
+++ calcPosition.js
@@ -2,39 +2,42 @@ module.exports.getCurrentBlockAndChank = function(x,y){
 	var playerBlockNumberX = Math.floor(x/64);
 	var playerBlockNumberY = Math.floor(y/64);
 
-	playerBlockNumberX = Math.abs(x) < 64? 1: playerBlockNumberX;
-	playerBlockNumberY = Math.abs(y) < 64? 1: playerBlockNumberY;
+	// playerBlockNumberX = Math.abs(x) < 64? 1: playerBlockNumberX;
+	// playerBlockNumberY = Math.abs(y) < 64? 1: playerBlockNumberY;
 
 	var blockX = Math.floor(Math.abs(playerBlockNumberX) % 16);
 	var blockY = Math.floor(Math.abs(playerBlockNumberY) % 16);
 
-	var numberChunkX;
-	var numberChunkY;
+	var numberChunkX = null;
+	var numberChunkY = null;
 
 	if(x<=0){
 		playerBlockNumberX--;
 		blockX = 15 - blockX;
 	}
 
-	if(y<=0){
-		playerBlockNumberY--;
+	if(y>=0){
 		blockY = 15 - blockY;
 	}
+
+	if(y<0){
+		playerBlockNumberY--;
+	}
 // calc X
-	if(playerBlockNumberX<=16 && playerBlockNumberX>0){
-		numberChunkX = 1;
+	if((playerBlockNumberX<=16 && playerBlockNumberX>0) || (x<64 && x>0)){
+		numberChunkX = 0;
 	}
 	if(playerBlockNumberX<=32 && playerBlockNumberX>16){
-		numberChunkX = 2;
+		numberChunkX = 1;
 	}
 	if(playerBlockNumberX<=48 && playerBlockNumberX>32){
-		numberChunkX = 3;
+		numberChunkX = 2;
 	}
 	if(playerBlockNumberX<=64 && playerBlockNumberX>48){
-		numberChunkX = 4;
+		numberChunkX = 3;
 	}
 	if(playerBlockNumberX<=0 && playerBlockNumberX>-16){
-		numberChunkX = -1;
+		numberChunkX = numberChunkX!=null? numberChunkX: -1;
 	}
 	if(playerBlockNumberX<=-16 && playerBlockNumberX>-32){
 		numberChunkX = -2;
@@ -46,7 +49,7 @@ module.exports.getCurrentBlockAndChank = function(x,y){
 		numberChunkX = -4;
 	}
 // calc Y
-	if(playerBlockNumberY<=16 && playerBlockNumberY>0){
+	if((playerBlockNumberY<=16 && playerBlockNumberY>0) || (y<64 && y>0)){
 		numberChunkY = -1;
 	}
 	if(playerBlockNumberY<=32 && playerBlockNumberY>16){
@@ -59,16 +62,16 @@ module.exports.getCurrentBlockAndChank = function(x,y){
 		numberChunkY = -4;
 	}
 	if(playerBlockNumberY<=0 && playerBlockNumberY>-16){
-		numberChunkY = 1;
+		numberChunkY = numberChunkY!=null? numberChunkY: 0;
 	}
 	if(playerBlockNumberY<=-16 && playerBlockNumberY>-32){
-		numberChunkY = 2;
+		numberChunkY = 1;
 	}
 	if(playerBlockNumberY<=-32 && playerBlockNumberY>-48){
-		numberChunkY = 3;
+		numberChunkY = 2;
 	}
 	if(playerBlockNumberY<=-48 && playerBlockNumberY>-64){
-		numberChunkY = 4;
+		numberChunkY = 3;
 	}
 
 	var result = {
