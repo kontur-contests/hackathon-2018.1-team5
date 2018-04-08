18c9f9e93ea841fd69ed7b752b2d5de9c9a7f194
aunikitin <sanya_nikiti@mail.ru>
2018-04-08 09:15:09 +0500

    calc

--- calcPosition.js
+++ calcPosition.js
@@ -1,76 +1,81 @@
 module.exports.getCurrentBlockAndChank = function(x,y){
-	var playerBlockNumberX = Math.floor(x/64);
-	var playerBlockNumberY = Math.floor(y/64);
+	var playerBlockNumberX = Math.floor(Math.abs(x)/64);
+	var playerBlockNumberY = Math.floor(Math.abs(y)/64);
 
 	// playerBlockNumberX = Math.abs(x) < 64? 1: playerBlockNumberX;
 	// playerBlockNumberY = Math.abs(y) < 64? 1: playerBlockNumberY;
 
-	var blockX = Math.floor(Math.abs(playerBlockNumberX) % 16);
-	var blockY = Math.floor(Math.abs(playerBlockNumberY) % 16);
+	var blockX = Math.abs(playerBlockNumberX) % 16;
+	var blockY = Math.abs(playerBlockNumberY) % 16;
 
-	var numberChunkX = null;
-	var numberChunkY = null;
-
-	if(x<=0){
-		playerBlockNumberX--;
-		blockX = 15 - blockX;
-	}
-
-	if(y>=0){
+	if (y > 0) {
 		blockY = 15 - blockY;
 	}
 
-	if(y<0){
-		playerBlockNumberY--;
+	if (x < 0) {
+		blockX = 15 - blockX;
 	}
+	var numberChunkX = null;
+	var numberChunkY = null;
+
+	// if(x<=0){
+	// 	playerBlockNumberX--;
+	// 	blockX = 15 - blockX;
+	// }
+
+	// if(y<0){
+	// 	blockY = 15 - blockY;
+	// 	playerBlockNumberY++;
+	// }
 // calc X
-	if((playerBlockNumberX<=16 && playerBlockNumberX>0) || (x<64 && x>0)){
+	if(playerBlockNumberX<=16 && playerBlockNumberX>0 && x>0){
+	// if((playerBlockNumberX<=16 && playerBlockNumberX>0) || (x<64 && x>0)){
 		numberChunkX = 0;
 	}
-	if(playerBlockNumberX<=32 && playerBlockNumberX>16){
+	if(playerBlockNumberX<=32 && playerBlockNumberX>16 && x>0){
 		numberChunkX = 1;
 	}
-	if(playerBlockNumberX<=48 && playerBlockNumberX>32){
+	if(playerBlockNumberX<=48 && playerBlockNumberX>32 && x>0){
 		numberChunkX = 2;
 	}
-	if(playerBlockNumberX<=64 && playerBlockNumberX>48){
+	if(playerBlockNumberX<=64 && playerBlockNumberX>48 && x>0){
 		numberChunkX = 3;
 	}
-	if(playerBlockNumberX<=0 && playerBlockNumberX>-16){
-		numberChunkX = numberChunkX!=null? numberChunkX: -1;
+	if(playerBlockNumberX<=16 && playerBlockNumberX>0 && x<=0){
+		numberChunkX = -1;
 	}
-	if(playerBlockNumberX<=-16 && playerBlockNumberX>-32){
+	if(playerBlockNumberX>16 && playerBlockNumberX<=32 && x<0){
 		numberChunkX = -2;
 	}
-	if(playerBlockNumberX<=-32 && playerBlockNumberX>-48){
+	if(playerBlockNumberX<=48 && playerBlockNumberX>32 && x<0){
 		numberChunkX = -3;
 	}
-	if(playerBlockNumberX<=-48 && playerBlockNumberX>-64){
+	if(playerBlockNumberX<=64 && playerBlockNumberX>48 && x<0){
 		numberChunkX = -4;
 	}
 // calc Y
-	if((playerBlockNumberY<=16 && playerBlockNumberY>0) || (y<64 && y>0)){
+	if((playerBlockNumberY<=16 && playerBlockNumberY>0) && (y>=0)){
 		numberChunkY = -1;
 	}
-	if(playerBlockNumberY<=32 && playerBlockNumberY>16){
+	if(playerBlockNumberY<=32 && playerBlockNumberY>16 && (y>0)){
 		numberChunkY = -2;
 	}
-	if(playerBlockNumberY<=48 && playerBlockNumberY>32){
+	if(playerBlockNumberY<=48 && playerBlockNumberY>32 && (y>0)){
 		numberChunkY = -3;
 	}
-	if(playerBlockNumberY<=64 && playerBlockNumberY>48){
+	if(playerBlockNumberY<=64 && playerBlockNumberY>48 && (y>0)){
 		numberChunkY = -4;
 	}
-	if(playerBlockNumberY<=0 && playerBlockNumberY>-16){
-		numberChunkY = numberChunkY!=null? numberChunkY: 0;
+	if((playerBlockNumberY<16 && playerBlockNumberY>=0) && (y<0)){
+		numberChunkY = 0;
 	}
-	if(playerBlockNumberY<=-16 && playerBlockNumberY>-32){
+	if(playerBlockNumberY<32 && playerBlockNumberY>=16 && (y<0)){
 		numberChunkY = 1;
 	}
-	if(playerBlockNumberY<=-32 && playerBlockNumberY>-48){
+	if(playerBlockNumberY<48 && playerBlockNumberY>=32 && (y<0)){
 		numberChunkY = 2;
 	}
-	if(playerBlockNumberY<=-48 && playerBlockNumberY>-64){
+	if(playerBlockNumberY<64 && playerBlockNumberY>=48 && (y<0)){
 		numberChunkY = 3;
 	}
 
